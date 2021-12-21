152fffd2e8d3b0e22eed48d08cceb904fe2a3e22
BuildTools <unconfigured@null.spigotmc.org>
2021-12-21 18:49:52 -0500

    ahhhh

--- app/helper/accessAnswers.py
+++ app/helper/accessAnswers.py
@@ -9,28 +9,34 @@ import ssl
 
 def newAnswer(question):
     url = "https://api.m3o.com/v1/answer/Question"
-    values = {
+
+    header = {
         'Content-Type' : 'application/json',
-        'Authorization' : 'Bearer OTEzNzY2ZDYtMmI2OC00MDE5LWIwMTktNzVhMjg2OGUyOTNk',
+        'Authorization' : 'Bearer ZGRlNjhhYWQtN2UyOS00ZTY5LWI2OTctZGU0ZmIwNWY2NThh',
     }
-    data = {"query" : "microsoft"}
 
-    header = urllib.parse.urlencode(values)
-    header = header.encode('ascii') # data should be bytes
+    data = {"query" : question}
+    data = urllib.parse.urlencode(data)
+    data = data.encode('ascii') # data should be bytes
 
-    req = urllib.request.Request(url, header, data)
+    req = urllib.request.Request(url, headers=header, data = data)
+    # req.add_header('Content-Type', 'application/json')
+    # req.add_header('Authorization', 'Bearer ZGRlNjhhYWQtN2UyOS00ZTY5LWI2OTctZGU0ZmIwNWY2NThh')
+    # print(req)
+
     try: urllib.request.urlopen(req)
     except urllib.error.URLError as e:
         print(e.reason)
+        return 0
 
-    # answerUrl = urllib.request.urlopen(req)
-    # answer = json.loads(answerUrl.read())
-
-    return 0
+    answerUrl = urllib.request.urlopen(req)
+    answer = json.loads(answerUrl.read())
+    return answer
 
 def main():
     ssl._create_default_https_context = ssl._create_unverified_context
     x = newAnswer("What is the meaning of life?")
+    print(x)
 
 if __name__ == "__main__":
     main()
