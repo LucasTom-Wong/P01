48f239fb8c5bf771fd38df1fb286cba72f509787
Julia Nelson <90808935+Lia-Nelson@users.noreply.github.com>
2022-01-04 21:22:56 -0500

    Update test.py

--- app/data/test.py
+++ app/data/test.py
@@ -1,9 +1,7 @@
 # Tests the databases
 import sys
 from os import remove, path
-# adds the repo to the sys paths. Gets abs path, gets parent directory, then the parent directory of that to get repo directory.
 
-sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from databases import Databases
 import random
 
